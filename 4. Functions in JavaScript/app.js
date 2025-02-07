b46b034e1fa363696c5fa1fdd7c94ee37175ff67
Andrew V D <andrewvd31@gmail.com>
2025-02-07 07:44:21 +0530

    Update v1.2

---
+++ 4. Functions in JavaScript/app.js
@@ -0,0 +1,14 @@
+function makeCoffee(sugar, milk) {
+    console.log("Add powder in the coffee");
+    console.log("Add " + milk + " litre of milk to it");
+    console.log("Add " + sugar + " spoons of sugar");
+    console.log("Mix it well. Yout hot coffee is ready!!!");
+}
+
+makeCoffee(2, 1);
+/* 
+    Add powder in the coffee
+    Add 1 litre of milk to it
+    Add 2 spoons of sugar
+    Mix it well. Yout hot coffee is ready!!!
+*/
